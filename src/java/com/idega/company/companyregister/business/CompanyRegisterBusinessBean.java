00f3c1da56d682213f212435a5d0002f6ec0c25e
Valdas Žemaitis <valdas@idega.com>
2019-03-27 15:11:53 +0200

    Making sure company is created when needed

--- src/java/com/idega/company/companyregister/business/CompanyRegisterBusinessBean.java
+++ src/java/com/idega/company/companyregister/business/CompanyRegisterBusinessBean.java
@@ -11,6 +11,7 @@ import javax.ejb.FinderException;
 import com.idega.business.IBOLookupException;
 import com.idega.business.IBORuntimeException;
 import com.idega.business.IBOServiceBean;
+import com.idega.company.business.CompanyBusiness;
 import com.idega.company.data.Company;
 import com.idega.company.data.CompanyHome;
 import com.idega.company.data.IndustryCode;
@@ -62,296 +63,271 @@ public class CompanyRegisterBusinessBean extends IBOServiceBean implements Compa
 			String industryCode,
 			String unregistrationType,
 			String unregistrationDate,
-			String banMarking) {
+			String banMarking
+	) {
+		Company company = getEntryByPersonalId(personal_id);
+		try {
+			if (company == null) {
+				CompanyBusiness companyBusiness = getServiceInstance(CompanyBusiness.class);
+				company = companyBusiness.storeCompany(name, personal_id);
+			}
+		} catch (Exception e) {
+			logger.log(Level.SEVERE, "Exception while creating company with personal ID " + personal_id + " and name " + name, e);
+			return false;
+		}
 
-			Company company_registry = getEntryByPersonalId(personal_id);
+		if (company == null) {
+			return false;
+		}
 
+		if (company.getPersonalID() == null || company.getPersonalID().equals("")) {
+			company.setPersonalID(personal_id);
+		}
+
+		if (banMarking != null && !banMarking.trim().equals("")) {
+			company.setBanMarking(banMarking.trim());
+		}
+		if (operation != null && !operation.trim().equals("")) {
+			company.setOperation(operation.trim());
+		}
+		if (orderAreaForName != null && !orderAreaForName.trim().equals("")) {
+			company.setOrderAreaForName(orderAreaForName.trim());
+		}
+		if (vatNumber != null && !vatNumber.trim().equals("")) {
+			company.setVATNumber(vatNumber.trim());
+		}
+		if (name != null && !name.trim().equals("")) {
+			company.setName(name.trim());
+		}
+
+		UserBusiness userBusiness = getUserBusiness();
+		if (userBusiness == null) {
+			logger.log(Level.SEVERE, "Could not retrieve UserBusiness service bean");
+			return false;
+		}
+
+		if (!StringUtil.isEmpty(personal_id) && !StringUtil.isEmpty(name)) {
+			User companyUser = null;
 			try {
-				if (company_registry == null) {
-					company_registry = getCompanyRegisterHome().create();
-				}
-			} catch(Exception re) {
-				logger.log(Level.SEVERE, "Exception while creating company register entry", re);
-				return false;
-			}
+				companyUser = userBusiness.getUser(personal_id);
+			} catch (Exception e) {}
+			if (companyUser != null) {
+				companyUser.setFirstName(name);
+				companyUser.setMiddleName(null);
+				companyUser.setLastName(null);
+				companyUser.setDisplayName(name);
+				companyUser.store();
 
-			if(company_registry.getPersonalID() == null || company_registry.getPersonalID().equals("")) {
-				company_registry.setPersonalID(personal_id);
-			}
-
-			if(banMarking != null && !banMarking.trim().equals("")) {
-				company_registry.setBanMarking(banMarking.trim());
-			}
-			if(operation != null && !operation.trim().equals("")) {
-				company_registry.setOperation(operation.trim());
-			}
-			if(orderAreaForName != null && !orderAreaForName.trim().equals("")) {
-				company_registry.setOrderAreaForName(orderAreaForName.trim());
-			}
-			if(vatNumber != null && !vatNumber.trim().equals("")) {
-				company_registry.setVATNumber(vatNumber.trim());
-			}
-			if(name != null && !name.trim().equals("")) {
-				company_registry.setName(name.trim());
-			}
-
-			UserBusiness userBusiness = getUserBusiness();
-			if(userBusiness == null) {
-				logger.log(Level.SEVERE, "Could not retrieve UserBusiness service bean");
-				return false;
-			}
-
-			if (!StringUtil.isEmpty(personal_id) && !StringUtil.isEmpty(name)) {
-				User companyUser = null;
 				try {
-					companyUser = userBusiness.getUser(personal_id);
-				} catch (Exception e) {}
-				if (companyUser != null) {
-					companyUser.setFirstName(name);
-					companyUser.setMiddleName(null);
-					companyUser.setLastName(null);
-					companyUser.setDisplayName(name);
-					companyUser.store();
-
-					try {
-						userBusiness.updateUsersMainAddressByFullAddressString(companyUser, address);
-					} catch (Exception e) {
-						getLogger().log(Level.WARNING, "Error updating main address for company user " + companyUser + ": " + address, e);
-					}
-					try {
-						AddressTypeHome addressTypeHome = (AddressTypeHome) IDOLookup.getHome(AddressType.class);
-						AddressType workAddressType = addressTypeHome.findAddressType2();
-						userBusiness.updateUsersAddressByFullAddressString(companyUser, address, workAddressType);
-					} catch (Exception e) {
-						getLogger().log(Level.WARNING, "Error updating work address for company user " + companyUser + ": " + address, e);
-					}
+					userBusiness.updateUsersMainAddressByFullAddressString(companyUser, address);
+				} catch (Exception e) {
+					getLogger().log(Level.WARNING, "Error updating main address for company user " + companyUser + ": " + address, e);
+				}
+				try {
+					AddressTypeHome addressTypeHome = (AddressTypeHome) IDOLookup.getHome(AddressType.class);
+					AddressType workAddressType = addressTypeHome.findAddressType2();
+					userBusiness.updateUsersAddressByFullAddressString(companyUser, address, workAddressType);
+				} catch (Exception e) {
+					getLogger().log(Level.WARNING, "Error updating work address for company user " + companyUser + ": " + address, e);
 				}
 			}
+		}
 
-			User ceo = null;
+		User ceo = null;
+		try {
+			if(ceoId != null) {
+				ceoId = ceoId.trim();
+				if(!ceoId.equals("")) {
+					ceo = userBusiness.getUser(ceoId);
+				}
+			}
+		} catch(FinderException re) {
+		} catch(Exception re) {
+			logger.log(Level.SEVERE, "Could not find the user by ID " + ceoId + " for company's (" + personal_id + ") CEO", re);
+		}
+		boolean createNewCeo = false;
+		if (ceo != null) {
+			if (!ceoId.equals(ceo.getPersonalID())) {
+				createNewCeo = true;
+			}
+		}
+		if (ceo == null || createNewCeo) {
 			try {
-				if(ceoId != null) {
-					ceoId = ceoId.trim();
-					if(!ceoId.equals("")) {
-						ceo = userBusiness.getUser(ceoId);
+				ceo = getUserHome().create();
+				ceo.setFullName(name.trim());
+				ceo.setPersonalID(ceoId);
+				ceo.store();
+				company.setCEO(ceo);
+			} catch(Exception re) {
+				logger.log(Level.SEVERE, "Exception while creating a new user entry", re);
+				return false;
+			}
+		}
+
+		if (recipientPersonalId != null && !recipientPersonalId.trim().equals("")) {
+			company.setRecipientId(recipientPersonalId);
+		}
+		if (recipientName != null && !recipientName.trim().equals("")) {
+			company.setRecipientName(recipientName);
+		}
+
+		Address addressBean = company.getAddress();
+		if (addressBean == null) {
+			try {
+				addressBean = getAddressHome().create();
+				addressBean.setAddressType(getAddressHome().getAddressType1());
+			} catch(Exception re) {
+				logger.log(Level.SEVERE, "Exception while creating a new address entry", re);
+				return false;
+			}
+		}
+
+		AddressBusiness addressBusiness = getAddressBusiness();
+		if (addressBusiness == null) {
+			logger.log(Level.SEVERE, "Could not retrieve AddressBusiness service bean");
+			return false;
+		}
+		try {
+			if (address != null) {
+				address = address.trim();
+				if (!address.equals("")) {
+					addressBean.setStreetName(addressBusiness.getStreetNameFromAddressString(address));
+					addressBean.setStreetNumber(addressBusiness.getStreetNumberFromAddressString(address));
+				}
+			}
+		} catch(Exception re) {
+			logger.log(Level.SEVERE, "Exception while handling company address", re);
+		}
+
+		Commune communeBean = addressBean.getCommune();
+		if (communeBean == null) {
+			try {
+				if (commune != null) {
+					commune = commune.trim();
+					if(!commune.equals("")) {
+						communeBean = getCommuneHome().findByCommuneCode(commune);
 					}
 				}
 			} catch(FinderException re) {
 			} catch(Exception re) {
-				logger.log(Level.SEVERE, "Could not find the user", re);
+				logger.log(Level.SEVERE, "Exception while finding commune entry", re);
 			}
-			boolean createNewCeo = false;
-			if(ceo != null) {
-				if(!ceoId.equals(ceo.getPersonalID())) {
-					createNewCeo = true;
-				}
-			}
-			if(ceo == null || createNewCeo) {
-				try {
-					ceo = getUserHome().create();
-					ceo.setFullName(name.trim());
-					ceo.setPersonalID(ceoId);
-					ceo.store();
-					company_registry.setCEO(ceo);
-				} catch(Exception re) {
-					logger.log(Level.SEVERE, "Exception while creating a new user entry", re);
-					return false;
-				}
-			}
+		}
+		addressBean.setCommune(communeBean);
+		company.setLastChange(CompanyRegisterImportFile.getSQLDateFromStringFormat(dateOfLastChange.trim(), logger, RECORDS_DATE_FORMAT));
+		if (registerDate != null) {
+			company.setRegisterDate(CompanyRegisterImportFile.getSQLDateFromStringFormat(registerDate.trim(), logger, RECORDS_DATE_FORMAT));
+		}
+		if (unregistrationDate != null) {
+			company.setUnregisterDate(CompanyRegisterImportFile.getSQLDateFromStringFormat(unregistrationDate.trim(), logger, RECORDS_DATE_FORMAT));
+		}
 
-
-//			User recipient = null;
-//			try {
-//				if(recipientPersonalId != null) {
-//					recipientPersonalId = recipientPersonalId.trim();
-//					if(!recipientPersonalId.equals("")) {
-//						recipient = userBusiness.getUser(recipientPersonalId);
-//					}
-//				}
-//			} catch(FinderException re) {
-//			} catch(Exception re) {
-//				logger.log(Level.SEVERE, "Could not find the user", re);
-//			}
-//			if(recipient == null) {
-//				try {
-//					recipient = getUserHome().create();
-//				} catch(Exception re) {
-//					logger.log(Level.SEVERE, "Exception while creating a new user entry", re);
-//					return false;
-//				}
-//			}
-//			if(!recipientName.trim().equals("")) {
-//				recipient.setName(recipientName);
-//			}
-//			recipient.setPersonalID(recipientPersonalId);
-			if(recipientPersonalId != null && !recipientPersonalId.trim().equals("")) {
-				company_registry.setRecipientId(recipientPersonalId);
-			}
-			if(recipientName != null && !recipientName.trim().equals("")) {
-				company_registry.setRecipientName(recipientName);
-			}
-
-
-			Address addressBean = company_registry.getAddress();
-			if(addressBean == null) {
-				try {
-					addressBean = getAddressHome().create();
-					addressBean.setAddressType(getAddressHome().getAddressType1());
-				} catch(Exception re) {
-					logger.log(Level.SEVERE, "Exception while creating a new address entry", re);
-					return false;
-				}
-			}
-
-			AddressBusiness addressBusiness = getAddressBusiness();
-			if(addressBusiness == null) {
-				logger.log(Level.SEVERE, "Could not retrieve AddressBusiness service bean");
-				return false;
-			}
+		PostalCode postalCodeBean = addressBean.getPostalCode();
+		if (postalCodeBean == null) {
 			try {
-				if(address != null) {
-					address = address.trim();
-					if(!address.equals("")) {
-						addressBean.setStreetName(addressBusiness.getStreetNameFromAddressString(address));
-						addressBean.setStreetNumber(addressBusiness.getStreetNumberFromAddressString(address));
+				if (postalCode != null) {
+					postalCode = postalCode.trim();
+					if(!postalCode.equals("")) {
+						postalCodeBean = getPostalCodeHome().findByPostalCode(postalCode);
 					}
-				}
-			} catch(Exception re) {
-				logger.log(Level.SEVERE, "Exception while handling company address", re);
-			}
-
-			Commune communeBean = addressBean.getCommune();
-			if(communeBean == null) {
-				try {
-					if(commune != null) {
-						commune = commune.trim();
-						if(!commune.equals("")) {
-							communeBean = getCommuneHome().findByCommuneCode(commune);
-						}
-					}
-				} catch(FinderException re) {
-				} catch(Exception re) {
-					logger.log(Level.SEVERE, "Exception while finding commune entry", re);
-				}
-			}
-			addressBean.setCommune(communeBean);
-			company_registry.setLastChange(CompanyRegisterImportFile.getSQLDateFromStringFormat(dateOfLastChange.trim(), logger, RECORDS_DATE_FORMAT));
-			if (registerDate != null) {
-				company_registry.setRegisterDate(CompanyRegisterImportFile.getSQLDateFromStringFormat(registerDate.trim(), logger, RECORDS_DATE_FORMAT));
-			}
-			if (unregistrationDate != null) {
-				company_registry.setUnregisterDate(CompanyRegisterImportFile.getSQLDateFromStringFormat(unregistrationDate.trim(), logger, RECORDS_DATE_FORMAT));
-			}
-
-			PostalCode postalCodeBean = addressBean.getPostalCode();
-			if(postalCodeBean == null) {
-				try {
-					if(postalCode != null) {
-						postalCode = postalCode.trim();
-						if(!postalCode.equals("")) {
-							postalCodeBean = getPostalCodeHome().findByPostalCode(postalCode);
-						}
-					}
-				} catch(FinderException re) {
-				} catch(Exception re) {
-					logger.log(Level.SEVERE, "Exception while finding a postal code entry", re);
-				}
-			}
-			addressBean.setPostalCode(postalCodeBean);
-			addressBean.store();
-			try {
-				Collection<Address> addreses = company_registry.getGeneralGroup().getAddresses(getAddressHome().getAddressType1());
-				String pk1 = ((Integer) addressBean.getPrimaryKey()).toString();
-				boolean addressSet = false;
-				for(Iterator<Address> it = addreses.iterator(); it.hasNext(); ) {
-					Address adr = it.next();
-					String pk2 = ((Integer) adr.getPrimaryKey()).toString();
-					if(pk1.equals(pk2)) {
-						addressSet = true;
-						break;
-					}
-				}
-				if(!addressSet) {
-					company_registry.setAddress(addressBean);
-				}
-			} catch(Exception e) {
-				logger.log(Level.SEVERE, "Exception while all address entries for a company", e);
-			}
-
-			Commune legalCommune = company_registry.getLegalCommune();
-			if(legalCommune == null) {
-				try {
-					if(legalAddress != null) {
-						legalAddress = legalAddress.trim();
-						if(!legalAddress.equals("")) {
-							legalCommune = getCommuneHome().findByCommuneCode(legalAddress);
-						}
-					}
-				} catch(FinderException re) {
-				} catch(Exception re) {
-					logger.log(Level.SEVERE, "Exception while finding a commune entry", re);
-				}
-			}
-			company_registry.setLegalCommune(legalCommune);
-
-			Commune workCommune = company_registry.getWorkingArea();
-			if(workCommune == null) {
-				try {
-					if(workingArea != null) {
-						workingArea = workingArea.trim();
-						if(!workingArea.equals("")) {
-							workCommune = getCommuneHome().findByCommuneCode(workingArea);
-						}
-					}
-				} catch(FinderException re) {
-				} catch(Exception re) {
-					logger.log(Level.SEVERE, "Exception while finding a commune entry", re);
-				}
-			}
-			company_registry.setWorkingArea(workCommune);
-
-			try {
-				OperationForm operationFormBean = ((OperationFormHome) IDOLookup.getHome(OperationForm.class)).findOperationFormByUniqueCode(operationForm);
-				if(operationFormBean != null) {
-					company_registry.setOperationForm(operationFormBean);
 				}
 			} catch(FinderException re) {
 			} catch(Exception re) {
-				logger.log(Level.SEVERE, "Exception while finding a OperationForm entry", re);
+				logger.log(Level.SEVERE, "Exception while finding a postal code entry", re);
 			}
+		}
+		addressBean.setPostalCode(postalCodeBean);
+		addressBean.store();
+		try {
+			Collection<Address> addreses = company.getGeneralGroup().getAddresses(getAddressHome().getAddressType1());
+			String pk1 = ((Integer) addressBean.getPrimaryKey()).toString();
+			boolean addressSet = false;
+			for (Iterator<Address> it = addreses.iterator(); it.hasNext(); ) {
+				Address adr = it.next();
+				String pk2 = ((Integer) adr.getPrimaryKey()).toString();
+				if (pk1.equals(pk2)) {
+					addressSet = true;
+					break;
+				}
+			}
+			if (!addressSet) {
+				company.setAddress(addressBean);
+			}
+		} catch(Exception e) {
+			logger.log(Level.SEVERE, "Exception while all address entries for a company", e);
+		}
 
+		Commune legalCommune = company.getLegalCommune();
+		if (legalCommune == null) {
 			try {
-				IndustryCode industryCodeBean = ((IndustryCodeHome) IDOLookup.getHome(IndustryCode.class)).findIndustryByUniqueCode(industryCode);
-				if(industryCodeBean != null) {
-					company_registry.setIndustryCode(industryCodeBean);
+				if (legalAddress != null) {
+					legalAddress = legalAddress.trim();
+					if (!legalAddress.equals("")) {
+						legalCommune = getCommuneHome().findByCommuneCode(legalAddress);
+					}
 				}
 			} catch(FinderException re) {
 			} catch(Exception re) {
-				logger.log(Level.SEVERE, "Exception while finding a IndustryCode entry", re);
+				logger.log(Level.SEVERE, "Exception while finding a commune entry", re);
 			}
+		}
+		company.setLegalCommune(legalCommune);
 
+		Commune workCommune = company.getWorkingArea();
+		if (workCommune == null) {
 			try {
-
-				UnregisterType unregisterTypeBean = ((UnregisterTypeHome) IDOLookup.getHome(UnregisterType.class)).findUnregisterTypeByUniqueCode(unregistrationType);
-
-				if(unregisterTypeBean != null) {
-					company_registry.setUnregisterType(unregisterTypeBean);
+				if (workingArea != null) {
+					workingArea = workingArea.trim();
+					if (!workingArea.equals("")) {
+						workCommune = getCommuneHome().findByCommuneCode(workingArea);
+					}
 				}
-			} catch (FinderException e) {
+			} catch(FinderException re) {
 			} catch(Exception re) {
-				logger.log(Level.SEVERE, "Exception while finding a UnregisterType entry", re);
+				logger.log(Level.SEVERE, "Exception while finding a commune entry", re);
 			}
-			company_registry.store();
+		}
+		company.setWorkingArea(workCommune);
 
-			return true;
+		try {
+			OperationForm operationFormBean = ((OperationFormHome) IDOLookup.getHome(OperationForm.class)).findOperationFormByUniqueCode(operationForm);
+			if (operationFormBean != null) {
+				company.setOperationForm(operationFormBean);
+			}
+		} catch(FinderException re) {
+		} catch(Exception re) {
+			logger.log(Level.SEVERE, "Exception while finding a OperationForm entry", re);
+		}
+
+		try {
+			IndustryCode industryCodeBean = ((IndustryCodeHome) IDOLookup.getHome(IndustryCode.class)).findIndustryByUniqueCode(industryCode);
+			if (industryCodeBean != null) {
+				company.setIndustryCode(industryCodeBean);
+			}
+		} catch(FinderException re) {
+		} catch(Exception re) {
+			logger.log(Level.SEVERE, "Exception while finding a IndustryCode entry", re);
+		}
+
+		try {
+			UnregisterType unregisterTypeBean = ((UnregisterTypeHome) IDOLookup.getHome(UnregisterType.class)).findUnregisterTypeByUniqueCode(unregistrationType);
+
+			if (unregisterTypeBean != null) {
+				company.setUnregisterType(unregisterTypeBean);
+			}
+		} catch (FinderException e) {
+		} catch(Exception re) {
+			logger.log(Level.SEVERE, "Exception while finding a UnregisterType entry", re);
+		}
+		company.store();
+
+		return true;
 	}
 
 	protected CompanyHome getCompanyRegisterHome() throws RemoteException {
 
 		return (CompanyHome) IDOLookup.getHome(Company.class);
 	}
-
-
 
 	protected UserHome getUserHome() throws RemoteException {
 		return (UserHome) IDOLookup.getHome(User.class);
