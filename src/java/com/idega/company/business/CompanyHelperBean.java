246b4eb349c50f421aa47f52f7b06158fea697d2
Valdas Žemaitis <valdas@idega.com>
2019-11-14 11:09:16 +0200

    Checking for email by company user

--- src/java/com/idega/company/business/CompanyHelperBean.java
+++ src/java/com/idega/company/business/CompanyHelperBean.java
@@ -1,6 +1,8 @@
 package com.idega.company.business;
 
 import java.rmi.RemoteException;
+import java.util.Collection;
+import java.util.Iterator;
 import java.util.logging.Level;
 
 import javax.ejb.FinderException;
@@ -13,11 +15,18 @@ import com.idega.business.IBOLookup;
 import com.idega.business.IBOLookupException;
 import com.idega.company.data.Company;
 import com.idega.core.business.DefaultSpringBean;
+import com.idega.core.contact.data.Email;
 import com.idega.core.location.data.Address;
+import com.idega.data.IDOLookup;
 import com.idega.idegaweb.IWMainApplication;
 import com.idega.user.bean.UserDataBean;
 import com.idega.user.business.CompanyHelper;
 import com.idega.user.business.UserApplicationEngine;
+import com.idega.user.business.UserBusiness;
+import com.idega.user.data.MetadataConstants;
+import com.idega.user.data.User;
+import com.idega.user.data.UserHome;
+import com.idega.util.ListUtil;
 import com.idega.util.StringUtil;
 import com.idega.util.expression.ELUtil;
 
@@ -42,12 +51,45 @@ public class CompanyHelperBean extends DefaultSpringBean implements CompanyHelpe
 		companyInfo.setGroupId((Integer) company.getPrimaryKey());
 
 		try {
-			getUserAppEngine().fillUserInfo(companyInfo, company.getPhone(), company.getEmail(), company.getAddress());
+			Email email = company.getEmail();
+			if (email == null) {
+				Collection<User> users = null;
+				try {
+					UserHome userHome = (UserHome) IDOLookup.getHome(User.class);
+					users = userHome.findUsersByMetaData(MetadataConstants.USER_REAL_COMPANY_META_DATA_KEY, company.getPrimaryKey().toString());
+				} catch (Exception e) {}
+				email = getEmail(users);
+			}
+			if (email == null) {
+				UserBusiness userBusiness = getServiceInstance(UserBusiness.class);
+				Collection<User> users = null;
+				try {
+					users = userBusiness.getUsersInGroup(companyInfo.getGroupId());
+				} catch (Exception e) {}
+				email = getEmail(users);
+			}
+
+			getUserAppEngine().fillUserInfo(companyInfo, company.getPhone(), email, company.getAddress());
 		} catch(Exception e) {
 			getLogger().log(Level.WARNING, "Error filling company (personal ID: " + companyPersonalId + ") info!", e);
 		}
 
 		return companyInfo;
+	}
+
+	private Email getEmail(Collection<User> users) {
+		if (ListUtil.isEmpty(users)) {
+			return null;
+		}
+
+		Email email = null;
+		for (Iterator<User> iter = users.iterator(); (email == null && iter.hasNext());) {
+			User user = iter.next();
+			try {
+				email = user.getUsersEmail();
+			} catch (Exception e) {}
+		}
+		return email;
 	}
 
 	private UserApplicationEngine getUserAppEngine() {
